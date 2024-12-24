89e60d730cf76511c85743f31c029716e20d2f8c
Your Name <bhaisareshivani509@gmail.com>
2024-12-24 17:06:59 +0530

    Initial commit for new project

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ btn.addEventListener("click", function () {
   } else {
     h5.innerHTML = "Strangers";
     h5.style.color = "palevioletred";
+
     btn.innerHTML = "Add Friend";
 
     check = 0;
@@ -19,9 +20,7 @@ btn.addEventListener("click", function () {
 // function ko toh run karne ke liye call karna padhta hai na?
 // Answer = Ye jo function hai jab hum click karte hai tab call hota hai har function ko call nahi karna padhta hai
 
-
-
 // Teen tarah ke function hote hai :-
-// 1. Ek hota hai jisko hum call kar ke bolte hai 
+// 1. Ek hota hai jisko hum call kar ke bolte hai
 // 2. Ek hota hai function woh jo is ke andar likh hai click hoga woh ye function
 //  run hoga automatically run nahi hoga jese hi click hoga function run ho jayega
